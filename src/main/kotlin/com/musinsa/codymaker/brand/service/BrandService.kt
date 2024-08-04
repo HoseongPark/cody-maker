fd9f85c3434700e8d0a9603ce6b245eecd364ea4
hoseong <ghtjd5689@gmail.com>
2024-08-04 18:35:34 +0900

    Brand 저장을 위한 응용 서비스 로직 구성 및 테스트 작성

---
+++ src/main/kotlin/com/musinsa/codymaker/brand/service/BrandService.kt
@@ -0,0 +1,25 @@
+package com.musinsa.codymaker.brand.service
+
+import com.musinsa.codymaker.brand.controller.model.BrandSaveReq
+import com.musinsa.codymaker.brand.domain.model.Brand
+import com.musinsa.codymaker.brand.domain.repository.BrandRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BrandService(private val brandRepo: BrandRepository) {
+
+    /**
+     * 브랜드 등록이후 사용되어야 필드에 대해 명시 되어 있지 않아.
+     * 등록된 브랜드의 ID를 반환 하도록 구성.
+     *
+     * - 해당 ID를 통해 브랜드에 대한 정보를 조회 할 수 있음.
+     * - 만약 추가적인 속성이 필요하다면, 추가적인 필드를 명시하여 반환하도록 수정.
+     */
+    fun saveBrand(req: BrandSaveReq): Long {
+        val brand = Brand(req.name, true)
+        val savedBrand = brandRepo.save(brand)
+        val id = savedBrand.id ?: throw RuntimeException("Brand ID is null")
+
+        return id
+    }
+}
---
+++ src/test/kotlin/com/musinsa/codymaker/brand/service/BrandServiceTest.kt
@@ -0,0 +1,42 @@
+package com.musinsa.codymaker.brand.service
+
+import com.musinsa.codymaker.brand.controller.model.BrandSaveReq
+import com.musinsa.codymaker.brand.domain.model.Brand
+import com.musinsa.codymaker.brand.domain.repository.BrandRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import io.mockk.every
+import io.mockk.mockk
+
+class BrandServiceTest : BehaviorSpec({
+
+    val brandRepo = mockk<BrandRepository>()
+
+    val brandService = BrandService(brandRepo)
+
+    Given("브랜드 등록") {
+        When("정상적으로 등록이 되었을 때") {
+            every { brandRepo.save(any()) } returns mockk<Brand> { every { id } returns 1L }
+
+            val brandSaveReq = BrandSaveReq("Nike")
+            val saveBrandId = brandService.saveBrand(brandSaveReq)
+
+            Then("브랜드 ID를 반환 한다.") {
+                saveBrandId shouldBe 1L
+            }
+        }
+
+        When("등록 중 에러가 발생했을 때") {
+            every { brandRepo.save(any()) } throws RuntimeException()
+
+            val brandSaveReq = BrandSaveReq("Nike")
+            val exception = shouldThrow<RuntimeException> { brandService.saveBrand(brandSaveReq) }
+
+            Then("예외를 발생시킨다.") {
+                exception.shouldBeInstanceOf<RuntimeException>()
+            }
+        }
+    }
+})

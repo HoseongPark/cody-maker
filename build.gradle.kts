6ad8e7fdf7167b97bc19f3602e578e9aff9f72a6
hoseong <ghtjd5689@gmail.com>
2024-08-04 13:05:39 +0900

    Kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     runtimeOnly("com.h2database:h2")
 
     // Test
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest:kotest-property:5.9.1")
+    testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")

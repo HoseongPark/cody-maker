ffac851cfdcaf3cfca6961e3a3413d539121da2e
hoseong <ghtjd5689@gmail.com>
2024-08-04 20:35:18 +0900

    주석 추가

--- src/main/kotlin/com/musinsa/codymaker/brand/service/BrandService.kt
+++ src/main/kotlin/com/musinsa/codymaker/brand/service/BrandService.kt
@@ -16,6 +16,7 @@ class BrandService(private val brandRepo: BrandRepository) {
      * - 만약 추가적인 속성이 필요하다면, 추가적인 필드를 명시하여 반환하도록 수정.
      */
     fun saveBrand(req: BrandSaveReq): Long {
+        // 브랜드 등록시 활성화 상태로 등록
         val brand = Brand(req.name, true)
         val savedBrand = brandRepo.save(brand)
         val id = savedBrand.id ?: throw RuntimeException("Brand ID is null")

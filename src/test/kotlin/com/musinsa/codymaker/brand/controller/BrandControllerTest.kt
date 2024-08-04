bba2a9c8a920eca510532adad4baaa629d690181
hoseong <ghtjd5689@gmail.com>
2024-08-05 02:37:24 +0900

    브랜드 API Test시 JsonString을 통한 Request를 통해 테스트 하도록 수정

--- src/test/kotlin/com/musinsa/codymaker/brand/controller/BrandControllerTest.kt
+++ src/test/kotlin/com/musinsa/codymaker/brand/controller/BrandControllerTest.kt
@@ -1,7 +1,5 @@
 package com.musinsa.codymaker.brand.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.musinsa.codymaker.brand.controller.model.BrandSaveReq
 import com.musinsa.codymaker.brand.service.BrandService
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.IsolationMode
@@ -19,24 +17,26 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @WebMvcTest(BrandController::class)
 class BrandControllerTest(
     @Autowired val mockMvc: MockMvc,
-    @Autowired val objectMapper: ObjectMapper,
     @MockkBean val brandService: BrandService
 ) : BehaviorSpec({
 
     isolationMode = IsolationMode.InstancePerLeaf
-
     extensions(listOf(SpringExtension))
 
-    Given("브랜드 등록 API 호출시") {
-        When("브랜드 저장 모델이 주어지면") {
-            val brandSaveReq = BrandSaveReq("Nike")
+    Given("브랜드 등록 API 요청시") {
+        When("유효한 브랜드 정보가 전달되면") {
+            val jsonString = """
+                {
+                    "name": "Nike"
+                }
+            """.trimIndent()
 
-            every { brandService.saveBrand(brandSaveReq) } returns 1L
+            every { brandService.saveBrand(any()) } returns 1L
 
             val perform = mockMvc.perform(
                 post("/brand")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(brandSaveReq))
+                    .content(jsonString)
             )
 
             Then("200 Status와, 브랜드 ID를 반환한다") {
@@ -46,15 +46,19 @@ class BrandControllerTest(
             }
         }
 
-        When("필수 값이 존재하지 않으면") {
-            val brandSaveReq = BrandSaveReq("")
+        When("유효 하지 않은 브랜드 정보가 전달 되면") {
+            val jsonString = """
+                {
+                    "name": ""
+                }
+            """.trimIndent()
 
-            every { brandService.saveBrand(brandSaveReq) } returns 1L
+            every { brandService.saveBrand(any()) } returns 1L
 
             val perform = mockMvc.perform(
                 post("/brand")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(brandSaveReq))
+                    .content(jsonString)
             )
 
             Then("400 Status를 반환한다") {

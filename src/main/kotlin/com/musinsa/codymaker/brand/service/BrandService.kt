1c55cefa00341475c7dc8227ffd54c0c117fef75
hoseong <ghtjd5689@gmail.com>
2024-08-04 22:50:51 +0900

    Brand 수정을 위한 응용 서비스 로직 구현

--- src/main/kotlin/com/musinsa/codymaker/brand/service/BrandService.kt
+++ src/main/kotlin/com/musinsa/codymaker/brand/service/BrandService.kt
@@ -1,9 +1,11 @@
 package com.musinsa.codymaker.brand.service
 
 import com.musinsa.codymaker.brand.controller.model.BrandSaveReq
+import com.musinsa.codymaker.brand.controller.model.BrandUpdateReq
 import com.musinsa.codymaker.brand.domain.model.Brand
 import com.musinsa.codymaker.brand.domain.repository.BrandRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BrandService(private val brandRepo: BrandRepository) {
@@ -15,12 +17,26 @@ class BrandService(private val brandRepo: BrandRepository) {
      * - 해당 ID를 통해 브랜드에 대한 정보를 조회 할 수 있음.
      * - 만약 추가적인 속성이 필요하다면, 추가적인 필드를 명시하여 반환하도록 수정.
      */
+    @Transactional
     fun saveBrand(req: BrandSaveReq): Long {
         // 브랜드 등록시 활성화 상태로 등록
         val brand = Brand(req.name, true)
         val savedBrand = brandRepo.save(brand)
-        val id = savedBrand.id ?: throw RuntimeException("Brand ID is null")
 
-        return id
+        // save 로직이 오류 없이 진행 되었을 경우 해당 ID는 항상 존재한다.
+        return savedBrand.id!!
+    }
+
+    /**
+     * 브랜드 수정 이후 사용되어야 필드에 대해 명시 되어 있지 않아.
+     * 수정된 브랜드의 ID를 반환 하도록 구성.
+     */
+    @Transactional
+    fun updateBrand(id: Long, req: BrandUpdateReq): Long {
+        val brand = brandRepo.getById(id)
+        brand.updateBrand(req.name, req.deleted)
+
+        // update 로직이 오류 없이 진행 되었을 경우 해당 ID는 항상 존재한다.
+        return brand.id!!
     }
 }
--- src/test/kotlin/com/musinsa/codymaker/brand/service/BrandServiceTest.kt
+++ src/test/kotlin/com/musinsa/codymaker/brand/service/BrandServiceTest.kt
@@ -1,6 +1,8 @@
 package com.musinsa.codymaker.brand.service
 
 import com.musinsa.codymaker.brand.controller.model.BrandSaveReq
+import com.musinsa.codymaker.brand.controller.model.BrandUpdateReq
+import com.musinsa.codymaker.brand.domain.infra.BrandJpaInfra
 import com.musinsa.codymaker.brand.domain.model.Brand
 import com.musinsa.codymaker.brand.domain.repository.BrandRepository
 import io.kotest.assertions.throwables.shouldThrow
@@ -9,10 +11,14 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.every
 import io.mockk.mockk
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-class BrandServiceTest : BehaviorSpec({
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BrandServiceTest(private val jpaInfra: BrandJpaInfra) : BehaviorSpec({
 
-    val brandRepo = mockk<BrandRepository>()
+    val brandRepo = BrandRepository(jpaInfra)
 
     val brandService = BrandService(brandRepo)
 
@@ -33,6 +39,30 @@ class BrandServiceTest : BehaviorSpec({
 
             val brandSaveReq = BrandSaveReq("Nike")
             val exception = shouldThrow<RuntimeException> { brandService.saveBrand(brandSaveReq) }
+
+            Then("예외를 발생시킨다.") {
+                exception.shouldBeInstanceOf<RuntimeException>()
+            }
+        }
+    }
+
+    Given("브랜드 수정") {
+        When("정상적으로 수정이 되었을 때") {
+            val saveBrand = brandRepo.save(Brand("Nike", true))
+
+            val brandUpdateReq = BrandUpdateReq("ABC", false)
+            val updateBrandId = brandService.updateBrand(saveBrand.id!!, brandUpdateReq)
+
+            Then("브랜드 ID를 반환 한다.") {
+                updateBrandId shouldBe 1L
+            }
+        }
+
+        When("수정 하려는 브랜드가 존재하지 않았을 때") {
+            brandRepo.save(Brand("Nike", true))
+
+            val brandUpdateReq = BrandUpdateReq("ABC", false)
+            val exception = shouldThrow<RuntimeException> { brandService.updateBrand(30L, brandUpdateReq) }
 
             Then("예외를 발생시킨다.") {
                 exception.shouldBeInstanceOf<RuntimeException>()

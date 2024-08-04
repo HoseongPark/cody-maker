43128d47a79dc704d2cfcd9833707e4e1a2671bf
hoseong <ghtjd5689@gmail.com>
2024-08-04 22:51:35 +0900

    Brand ID를 통해 도메인을 조회할 수 있도록 기능 추가

--- src/main/kotlin/com/musinsa/codymaker/brand/domain/repository/BrandRepository.kt
+++ src/main/kotlin/com/musinsa/codymaker/brand/domain/repository/BrandRepository.kt
@@ -2,10 +2,16 @@ package com.musinsa.codymaker.brand.domain.repository
 
 import com.musinsa.codymaker.brand.domain.infra.BrandJpaInfra
 import com.musinsa.codymaker.brand.domain.model.Brand
+import com.musinsa.codymaker.common.exception.NotFoundException
 import org.springframework.stereotype.Repository
 
 @Repository
 class BrandRepository(private val jpaInfra: BrandJpaInfra) {
 
     fun save(brand: Brand): Brand = jpaInfra.save(brand)
+
+    fun getById(id: Long): Brand {
+        val brand = jpaInfra.findById(id).orElseThrow { throw NotFoundException("Brand not found") }
+        return brand
+    }
 }
--- src/test/kotlin/com/musinsa/codymaker/brand/domain/repository/BrandRepositoryTest.kt
+++ src/test/kotlin/com/musinsa/codymaker/brand/domain/repository/BrandRepositoryTest.kt
@@ -2,18 +2,22 @@ package com.musinsa.codymaker.brand.domain.repository
 
 import com.musinsa.codymaker.brand.domain.infra.BrandJpaInfra
 import com.musinsa.codymaker.brand.domain.model.Brand
-import io.kotest.core.extensions.Extension
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.extensions.spring.SpringExtension
+import io.kotest.extensions.spring.SpringTestExtension
+import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.shouldBe
-import org.springframework.beans.factory.annotation.Autowired
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.TestPropertySource
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BrandRepositoryTest (private val jpaInfra: BrandJpaInfra) : BehaviorSpec({
+
+    extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
+    isolationMode = IsolationMode.InstancePerLeaf
 
     val brandRepository = BrandRepository(jpaInfra)
 
@@ -25,8 +29,34 @@ class BrandRepositoryTest (private val jpaInfra: BrandJpaInfra) : BehaviorSpec({
             Then("브랜드의 정보를 알 수 있다.") {
                 savedBrand.id shouldBe 1L
                 savedBrand.name shouldBe "Nike"
-                savedBrand.delete shouldBe true
+                savedBrand.deleted shouldBe true
             }
         }
+    }
+
+    Given("브랜드 조회") {
+        When("존재하는 브랜드의 정보를 조회 하면") {
+            val brand = Brand("Nike", true)
+            brandRepository.save(brand)
+
+            val foundBrand = brandRepository.getById(1L)
+
+            Then("브랜드의 정보를 알 수 있다.") {
+                foundBrand.id shouldBe 1L
+                foundBrand.name shouldBe "Nike"
+                foundBrand.deleted shouldBe true
+            }
+        }
+
+        When("존재하지 않는 브랜드의 정보를 조회 하면") {
+            val exception = shouldThrow<RuntimeException> {
+                brandRepository.getById(1L)
+            }
+
+            Then("예외가 발생한다.") {
+                exception.shouldBeInstanceOf<RuntimeException>()
+            }
+        }
+
     }
 })

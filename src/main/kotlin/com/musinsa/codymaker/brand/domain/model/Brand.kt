e92a504e9ecb767969897650474024c8b66f8848
hoseong <ghtjd5689@gmail.com>
2024-08-06 03:38:13 +0900

    삭제관련 비즈니스 규칙 추가

--- src/main/kotlin/com/musinsa/codymaker/brand/domain/model/Brand.kt
+++ src/main/kotlin/com/musinsa/codymaker/brand/domain/model/Brand.kt
@@ -4,12 +4,14 @@ import com.musinsa.codymaker.common.model.BaseEntity
 import jakarta.persistence.Entity
 
 @Entity
-class Brand(name: String, deleted: Boolean) : BaseEntity() {
+class Brand(name: String) : BaseEntity() {
 
     // 브랜드 이름은 중복되지 않은 유일한 이름이라고 가정한다.
     var name: String = name
+        private set
 
-    var deleted: Boolean = deleted
+    // 생성 시점에서는 삭제되지 않은 상태로 가정한다.
+    var deleted: Boolean = false
         private set
 
     fun update(name: String? = null) {
--- src/main/kotlin/com/musinsa/codymaker/product/domain/model/Product.kt
+++ src/main/kotlin/com/musinsa/codymaker/product/domain/model/Product.kt
@@ -9,6 +9,7 @@ import jakarta.persistence.Enumerated
 @Entity
 class Product(brandId: Long, category: Category, price: Int) : BaseEntity() {
 
+    // 브랜드의 경우 수정이 불가능하다로 가정하고 작성
     @Column(name = "brand_id")
     val brandId: Long = brandId
 
@@ -19,12 +20,20 @@ class Product(brandId: Long, category: Category, price: Int) : BaseEntity() {
     var price: Int = price
         private set
 
+    // 생성 시점에서는 삭제되지 않은 상태로 가정한다.
+    var deleted: Boolean = false
+        private set
+
     /**
      * 상품의 카테고리와, 가격을 수정 할 수 있다.
      */
     fun update(category: Category? = null, price: Int? = null) {
         category?.let { this.category = it }
         price?.let { this.price = it }
+    }
+
+    fun delete() {
+        this.deleted = true
     }
 
 }
